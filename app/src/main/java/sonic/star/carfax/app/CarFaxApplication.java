de65feedff5306704434d19781cca39f133053fc
Chirag Butani <chirag.butani1@gmail.com>
2019-10-16 19:29:48 -0400

    inital commit

---
+++ app/src/main/java/sonic/star/carfax/app/CarFaxApplication.java
@@ -0,0 +1,15 @@
+package sonic.star.carfax.app;
+
+import android.app.Application;
+
+/**
+ * Created by Chirag on 2019-10-16 at 19:16
+ * Project - CarFax
+ */
+public class CarFaxApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
